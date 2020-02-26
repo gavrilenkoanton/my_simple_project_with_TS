8f788a7175fc47dc36fa6c43ad68495f3e1e8cde
gavrilenkoanton <59771981+gavrilenkoanton@users.noreply.github.com>
2020-02-26 15:29:28 +0300

    add component Hello

---
+++ src/HelloComponent/Hello.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Hello.module.css';
+
+class Hello extends React.Component {
+
+    render = () => {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Hello;
